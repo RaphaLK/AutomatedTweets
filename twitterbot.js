9afc623ac579eae66c309b13531ba9570e22e2e5
RaphaLK <77198909+RaphaLK@users.noreply.github.com>
2024-01-09 22:33:45 -0800

    removed twit, switched to twitter api v2

--- twitterbot.js
+++ twitterbot.js
@@ -1,26 +1,38 @@
-const Twit = require('twit');
 require('dotenv').config()
+const https = require('https');
+const queryString = require('querystring');
+
+const Tweet = querystring.stringify({
+  'text': 'Twitter bot works!'
+});
 
 const config = {
   consumer_key: process.env.c_key,
   consumer_secret: process.env.cs_key,
   access_token: process.env.a_token,
   access_token_secret: process.env.as_token,
+
+  hostname: 'https://api.twitter.com',
+  port: 443,
+  path: '/2/tweets/',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Authorization': `OAuth oauth_consumer_key="${process.env.c_key}",oauth_token="${process.env.a_token}",oauth_signature_method="HMAC-SHA1",oauth_nonce="${Date.now()}",oauth_version="1.0",oauth_signature="${YOUR_GENERATED_SIGNATURE}"`
+  }
 }
 
-const T = new Twit(config);
 
-const postTweet = () => {
-  let Tweet = 'Twitter API config works!'
+const postTweet = async (Text) => {
 
-  T.post('statuses/updates', { status: Tweet }, (error, data, res) => {
-    if (error) {
-      console.log(error);
+  T.post('https://api.twitter.com/2/tweets', { status: `${Text}` }, (err, data, response) => {
+    if (err) {
+      console.log(err);
     }
     else {
-      console.log('Success');
+      console.log(data);
     }
   })
+
 }
-postTweet();
-setInterval(postTweet, 3600000)
+postTweet('Twitter bot works!');
